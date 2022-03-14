389a11f61debd8a74ca66cf1b632a75117dd3778
github_username <jacobrugano@gmail.com>
2022-03-14 19:55:36 +0300

    Created the login pages

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
+from atexit import register
 from pydoc import render_doc
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
@@ -8,7 +9,12 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
 
+    from .views import views
+    from .auth import auth
 
+
+    app.register_blueprint(views, url_prefix = ('/'))
+    app.register_blueprint(auth, url_prefix = ('/'))
 
 
 
