58dbeb3f5fae72107dd820fab908bfadf59c2d42
github_username <jacobrugano@gmail.com>
2022-03-15 15:50:20 +0300

    Added login to validate the info put by users as logins

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,8 @@
 from crypt import methods
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from .models import User
+
 
 auth = Blueprint("auth", __name__)
 
@@ -11,11 +14,32 @@ def login():
 
 @auth.route('/sign-up', methods= ['GET','POST'])
 def sign_up():
-    email = request.form.get("email")
-    username = request.form.get("username")
-    password1 = request.form.get("password1")
-    password2 = request.form.get("password2")
+    if request.method == "POST":
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
     
+        email_exists = User.query.filter_by(email=email).first()
+        username_exists = User.query.filter_by(username=username).first()
+        email.exists = User.query.filter_by(email=email).first()
+        email.exists = User.query.filter_by(email=email).first()
+        email.exists = User.query.filter_by(email=email).first()
+
+
+        if email_exists:
+            flash('Email already exists!', category='error')
+        elif username_exists:
+            flash('Username already exists!', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match!', category='error')
+        elif len(username) < 4:
+            flash('Username is too short!', category='error')
+        elif len(email) < 5:
+            flash('Email is too short!', category='error')
+        elif len(password1) < 7:
+            flash('Password is too short!', category='error')
+
     print(username)
     return render_template('signup.html')
 
