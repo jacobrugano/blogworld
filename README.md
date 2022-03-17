7a96d19a621f1061e65c3c8aa43fa82a9cfff06c
github_username <jacobrugano@gmail.com>
2022-03-17 11:38:14 +0300

    Added the readme.md file

--- README.md
+++ README.md
@@ -7,17 +7,48 @@ Jacob Maina
 This is a flask application that allows writers to post blogs, edit and delite blogs. It also allows users who have signed up to comment on the blogs that has been posted by a writer. It also allows a person to subscribed to recieve email everytime a new blog is posted by a writer.
 
 ### User Story
-A user can view the most recent posts.
-View and comment the blog posts on the site.
-A user should an email alert when a new post is made by joining a subscription.
-Register to be allowed to log in to the application
-A user sees random quotes on the site
-A writer can create a blog from the application and update or delete blogs I have created.
+1. A user can view the most recent posts.
+2. View and comment the blog posts on the site.
+3. A user should an email alert when a new post is made by joining a subscription.
+4. Register to be allowed to log in to the application
+5. A user sees random quotes on the site
+6. A writer can create a blog from the application and update or delete blogs I have created.
 
 ### Technology used
-Python3.6
-Flask
-Heroku
+1. Python3.9
+2. Flask
+3. Heroku CLI
+4. git CLI
+5. Markdown
+6. HTML
+7. CSS
+8. Bootstrap
+9. GitHub
+
+## Behaviour Driven Development (BDD)
+
+| Behaviour   | Input      | Output |
+| ------------- | ------------- | -------- |
+| user loads the application         | On page load        | users see various posts/blogs posted by bloggers |
+| user clicks on register button         | on click        | subscription page is loaded and user enters details for subscription  |
+| writer cllicks on sign up         | On page load    | sign up page is loaded and user can sign up to be a blogger |
+| writer clicks on crete blog        | on page load        | user is able to create blog on when the page loads  |
+| writes clicks on edit post       | edit page is loaded        | user is able to update the blog  |
+| writer clicks on delete post       | on page load        | the post is deleted  |
+| writer clicks on profile       | on page load         | the comment is deleted  |
+
+## Prerequisites
+Python3.6 and above
+
+## Setup/Installation Requirements
+1. git clone https://github.com/jacobrugano/blogworld.git.
+
+2. Note:You will need to git installed in your machine. You can install using the following command: $ sudo apt-get install git.
+3. After cloning, use command cd Blog-Post.git and open using your code editor.
+4. Create a vitual environment using the following command python3 -m venv --without-pip virtual
+5. Activate the virtual environment using the following command . virtual/bin/activate
+6. Run this command to interact with the application $python3.9 app.py server
+7. Run tests units using the this command $python --version(examplepython3.9 app.py`)
 
 ### Known Bugs
 There are no known bugs currently but pull requests are allowed incase you spot a bug
--- website/views.py
+++ website/views.py
@@ -1,18 +1,18 @@
-from crypt import methods
-from unicodedata import category
-from flask import Blueprint, render_template, request, flash, url_for, redirect
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Post, User, Comment
 from . import db
 
 views = Blueprint("views", __name__)
 
-@views.route('/')
-@views.route('/home')
+
+@views.route("/")
+@views.route("/home")
 @login_required
 def home():
     posts = Post.query.all()
     return render_template("home.html", user=current_user, posts=posts)
+
 
 @views.route("/create-post", methods=['GET', 'POST'])
 @login_required
@@ -21,41 +21,43 @@ def create_post():
         text = request.form.get('text')
 
         if not text:
-            flash('Blog area can not be empty!!', category="error")
+            flash('Blog cannot be empty', category='error')
         else:
             post = Post(text=text, author=current_user.id)
             db.session.add(post)
             db.session.commit()
-            flash('Blog successfully created!!', category="success")
+            flash('Blog created successfully!!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("create_post.html", user=current_user)
+    return render_template('create_post.html', user=current_user)
+
 
 @views.route("/delete-post/<id>")
 @login_required
 def delete_post(id):
-    post = Post.query.filter_by(id=id).first() #To check by ID if the post exists
+    post = Post.query.filter_by(id=id).first()
 
     if not post:
-        flash('Blog does not exist', category='error')
+        flash("Blog does not exist.", category='error')
     elif current_user.id != post.id:
-        flash('You do not have permission to delete this blog', category='error')
+        flash('You do not have permission to delete this Blog! Think twice!', category='error')
     else:
         db.session.delete(post)
         db.session.commit()
-        flash('Blog deleted. Think twice!!', category = "success")
+        flash('Post deleted.', category='success')
+
     return redirect(url_for('views.home'))
+
 
 @views.route("/posts/<username>")
 @login_required
 def posts(username):
-    user = User.query.filter_by(username=username).first() # To check if the username that the user typed actually exists.
-    
+    user = User.query.filter_by(username=username).first()
+
     if not user:
-        flash('No user with such a username in bloggit!!', category='error')
+        flash('No user with such username exists.', category='error')
         return redirect(url_for('views.home'))
 
-   # post = Post.query.filter_by(author=user.id).all() # To get blogs by a certain user
     posts = user.posts
     return render_template("posts.html", user=current_user, posts=posts, username=username)
 
@@ -66,7 +68,7 @@ def create_comment(post_id):
     text = request.form.get('text')
 
     if not text:
-        flash('Comment cannot be empty.', category='error')
+        flash('Comment area cannot be empty.', category='error')
     else:
         post = Post.query.filter_by(id=post_id)
         if post:
@@ -75,6 +77,22 @@ def create_comment(post_id):
             db.session.add(comment)
             db.session.commit()
         else:
-            flash('Post does not exist.', category='error')
+            flash('Blog does not currently exist!.', category='error')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not currently exist.', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permission to delete this comment!! Think twice brother!!', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
 
     return redirect(url_for('views.home'))
